6b9fe9839a79e49460964b567c0e378e038a8465
Austin Lee <inbox.austinlee@gmail.com>
2023-05-17 19:37:29 -0700

    add support for images

--- backend/src/controller/matrixController.ts
+++ backend/src/controller/matrixController.ts
@@ -4,13 +4,18 @@ import gm from "gm";
 import { matrix, realm } from "../index.js";
 import { ImageItem } from "../models/ImageSchema.js";
 import { matrixOptions } from "../models/matrixOptions.js";
-import { getCurrentImage, nextImage } from "./imageController.js";
+import {
+  getCurrentImage,
+  nextImage,
+  setCurrentByName,
+} from "./imageController.js";
 
 var running = false;
 let interval = null;
 
 export async function startMatrix() {
   running = true;
+  setCurrentByName("surprised-pikachu");
   let current: ImageItem = getCurrentImage();
   matrix.clear().brightness(current.brightness);
   let currFrame: number = 0;
@@ -60,20 +65,31 @@ async function loadImageAndScale(path: string) {
           .then(function (imageGif) {
             resolve(imageGif);
           })
-          .catch((err) => resolve(err));
+          .catch((err) => resolve(buffer));
       });
   });
 }
 
-function removeAlpha(frame: GifFrame) {
-  const height = frame.bitmap.height;
-  const width = frame.bitmap.width;
-  const newBuffer = new Uint8Array(width * 2 * height * 3);
+function removeAlpha(frame: GifFrame | Buffer) {
+  let height: number;
+  let width: number;
+  let newBuffer: Uint8Array;
+  let buf: Buffer;
+  if (frame instanceof GifFrame) {
+    height = frame.bitmap.height;
+    width = frame.bitmap.width;
+    newBuffer = new Uint8Array(width * 2 * height * 3);
+    buf = frame.bitmap.data;
+  } else {
+    height = matrixOptions.rows * 3;
+    width = matrixOptions.cols * 2;
+    newBuffer = new Uint8Array(width * 2 * height * 3);
+    buf = frame;
+  }
 
   let x = 0;
   let y = 0;
   let row = [];
-  let buf = frame.bitmap.data;
   for (let bi = 0; bi < buf.length; bi += 4) {
     const r = buf[bi];
     const g = buf[bi + 1];
